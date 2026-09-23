0b2d7eab2cf72a9c39985a158e15b7261cc244c7
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-23 16:31:47 +0500

    impl handleInput

--- main.go
+++ main.go
@@ -52,10 +52,15 @@ func (g *Game) draw() {
 	termbox.SetCell(g.width-1, 0, '┐', termbox.ColorWhite, termbox.ColorDefault)
 	termbox.SetCell(0, g.height-1, '└', termbox.ColorWhite, termbox.ColorDefault)
 	termbox.SetCell(g.width-1, g.height-1, '┘', termbox.ColorWhite, termbox.ColorDefault)
-	if len(g.snake) > 0 {
-		head := g.snake[0]
-		termbox.SetCell(head.x, head.y, '█', termbox.ColorGreen, termbox.ColorDefault)
+
+	for i, segment := range g.snake {
+		char := '○'
+		if i == 0 {
+			char = g.dir.ToRune()
+		}
+		termbox.SetCell(segment.x, segment.y, char, termbox.ColorGreen, termbox.ColorDefault)
 	}
+
 	scoreText := fmt.Sprintf("Score: %d Level: %d", g.score, g.level)
 	for i, char := range scoreText {
 		termbox.SetCell(i+2, 1, char, termbox.ColorYellow, termbox.ColorDefault)
@@ -63,14 +68,86 @@ func (g *Game) draw() {
 	termbox.Flush()
 }
 
+func (p Point) ToRune() rune {
+	switch {
+	case p.x == 0 && p.y == -1:
+		return '▲'
+	case p.x == 0 && p.y == 1:
+		return '▼'
+	case p.x == -1 && p.y == 0:
+		return '◀'
+	case p.x == 1 && p.y == 0:
+		return '▶'
+	default:
+		return '●'
+	}
+}
+
+func (g *Game) handleInput(ev termbox.Event) {
+	if ev.Type != termbox.EventKey {
+		return
+	}
+	switch ev.Key {
+	case termbox.KeyEsc:
+		close(g.quit)
+		return
+	}
+	var newDir Point
+	switch ev.Key {
+	case termbox.KeyArrowUp:
+		newDir = Point{x: 0, y: -1}
+	case termbox.KeyArrowDown:
+		newDir = Point{x: 0, y: 1}
+	case termbox.KeyArrowLeft:
+		newDir = Point{x: -1, y: 0}
+	case termbox.KeyArrowRight:
+		newDir = Point{x: 1, y: 0}
+	default:
+		switch ev.Ch {
+		case 'w', 'W':
+			newDir = Point{x: 0, y: -1}
+		case 's', 'S':
+			newDir = Point{x: 0, y: 1}
+		case 'a', 'A':
+			newDir = Point{x: -1, y: 0}
+		case 'd', 'D':
+			newDir = Point{x: 1, y: 0}
+		case 'q', 'Q':
+			close(g.quit)
+			return
+		default:
+			return
+		}
+	}
+	if newDir.x == -g.dir.x && newDir.y == -g.dir.y {
+		return
+	}
+	g.dir = newDir
+}
+
 func main() {
 	err := termbox.Init()
 	if err != nil {
-		fmt.Errorf(err.Error())
+		fmt.Println(err)
 		return
 	}
 	defer termbox.Close()
-
 	g := NewGame(40, 20)
+	events := make(chan termbox.Event)
+	go func() {
+		for {
+			ev := termbox.PollEvent()
+			events <- ev
+		}
+	}()
 	g.draw()
+	for {
+		select {
+		case ev := <-events:
+			g.handleInput(ev)
+			g.draw()
+		case <-g.quit:
+			return
+		}
+	}
 }
