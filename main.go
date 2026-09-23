f925c50ec7411ebcb6cedd68a42fea80787b0d6a
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-23 16:10:10 +0500

    impl NewGame

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Point struct {
 	x, y int
 }
@@ -16,8 +18,22 @@ type Game struct {
 	quit          chan struct{}
 }
 
-func NewGame() *Game {
-	return &Game{}
+func NewGame(width, height int) *Game {
+	return &Game{
+		snake: []Point{{x: width / 2, y: height / 2}},
+		dir: Point{
+			x: 1,
+			y: 0,
+		},
+		width:    width,
+		height:   height,
+		level:    1,
+		score:    0,
+		gameOver: false,
+		quit:     make(chan struct{}),
+	}
 }
 
-func main() {}
+func main() {
+	fmt.Println(NewGame(40, 20))
+}
