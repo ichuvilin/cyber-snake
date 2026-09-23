d8745506dba78d5a3efd519065178a391eb59fda
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-23 20:49:19 +0500

    impl logic

--- main.go
+++ main.go
@@ -221,6 +221,10 @@ func (g *Game) move() {
 	g.snake = append([]Point{newHead}, g.snake...)
 	if newHead.x == g.food.x && newHead.y == g.food.y {
 		g.score++
+		if g.score%5 == 0 {
+			g.level++
+			g.placeMalware()
+		}
 		g.placeFood()
 		return
 	}
