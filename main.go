a9a018a6e68d83b8753a4bc340e6d231448696d2
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-23 15:49:51 +0500

    create base struct

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
 package main
 
+type Point struct {
+	x, y int
+}
+
+type Game struct {
+	snake         []Point
+	food          Point
+	malware       []Point
+	dir           Point
+	score         int
+	level         int
+	gameOver      bool
+	width, height int
+	quit          chan struct{}
+}
+
+func NewGame() *Game {
+	return &Game{}
+}
+
 func main() {}
